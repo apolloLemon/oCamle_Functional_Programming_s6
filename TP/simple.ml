411d1af707a46ce8880342a8a27ba6e9bca521c8
Matthew Coyle <matthew@coyle.fr>
2020-01-23 11:18:00 +0100

    Simple Functions

--- TP/simple.ml
+++ TP/simple.ml
@@ -13,3 +13,33 @@ let isLower = function
 (* char -> bool *)
 
 (*Two Variables*)
+let moyenne x y = (x+.y)/.2.;;
+(*R -> R -> R*)
+
+let quotienReste = 
+	function dvd ->
+	function dvs ->
+		dvd/dvs :: dvd mod dvs :: []
+;;
+(* N -> N -> NList*)
+
+(*Local Declarations*)
+let puissance4 x = 
+	let carre y = y*y in 
+		carre x * carre x
+;;
+(*N->N*)
+
+let toUpper = 
+	function c ->
+	let cc = Char.code c in
+	let ac = Char.code 'a' in
+	let zc = Char.code 'z' in
+	let delta = ((Char.code 'a')-(Char.code 'A')) in 
+	let isLower = function 
+		a -> ((a>=ac)&&(a<=zc))
+	in if (isLower cc) 
+		then Char.chr (cc-delta)
+		else c 
+;;
+(*char -> char*)
