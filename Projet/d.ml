a15cb8fa0390ddbf5bf9f28908cc1a64575f0cd9
Matthew Coyle <matthew@coyle.fr>
2020-03-01 17:06:41 +0100

    parcours_prof complete

---
+++ Projet/d.ml
@@ -0,0 +1,98 @@
+(*type graphe = (int * int list) list;*)
+
+let graphe1 = [	(1, [6;7;8] 	) ; 
+				(2, [1;4] 		) ; 
+				(3, [2] 		) ; 
+				(4, [3;5] 		) ; 
+				(5, [1] 		) ; 
+				(6, [5;7] 		) ; 
+				(7, [] 			) ; 
+				(8, [6;7] 		)];;
+
+(* ('a * 'b)list -> 'a list *)
+let rec liste_sommet = function
+	[] 			-> []
+| 	(s,_)::r 	-> s::liste_sommet r
+;; 
+let liste_sommet g = 
+	List.fold_left (fun a (s,_) -> a@[s]) [] g
+;; 
+
+
+
+(* 'a -> ('a * 'b)list -> 'b list *)
+let rec liste_succ = function 
+	n -> function
+			[] 			-> failwith "404"
+		| 	(s,succ)::r -> if s=n 	then succ
+									else liste_succ n r
+;;
+(* ('a * 'b)list -> 'a -> 'b list *)
+let rec liste_succ g n = 
+	match g with
+			[] 			-> failwith "404"
+		| 	(s,succ)::r -> if s=n 	then succ
+									else liste_succ r n
+;;
+let rec liste_succ = function 
+			[] 			-> failwith "404"
+		| 	(s,succ)::r -> function n -> if s=n 	then succ
+													else liste_succ r n
+;;
+
+
+
+(* 'a -> ('b * 'a list) list -> 'b list *)
+let rec findPred = function 
+	n -> function
+		[]		->	[]
+| (s,succ)::r 	-> if List.exists (fun p -> if p=n then true else false) succ
+						then s::findPred n r
+						else findPred n r
+;;
+
+(* ('a * 'a list) list -> ('a * 'a list) list *)
+let inverser g =
+	List.fold_left 
+	(fun a (s,succ) -> a@[(s,findPred s g)])
+	[] g 
+;;
+
+
+(*
+(p,i) est un couple (parcours , information)
+C'est 'case memoire' qui va voyager a travers les fold_left
+
+
+sommet graphe parcours informations
+*)
+let rec depthProbe s g (p,i)=
+	let dPLog s (p,i) = (s::p,i) in (* rajouter un sommet a' la partie parcours du 'couple information' *)
+	if (List.mem s i)
+		then (p,i) (*On est deja passe' par ici, on retourne par d'ou' on viens avec nos informations*)
+		else dPLog s (* rajoute ce sommet dans le parcours retourne' par la suite*)
+			(List.fold_left (* On relance sur tout les successeurs *)
+				(fun (p,i) s ->
+					depthProbe s g (p,i))
+				(p,s::i) (*on rejoute l'information comme quoi on est passe' par s*)
+				(liste_succ g s) (*Liste des succ pour le fold_left*)
+			)
+;;
+
+
+
+let parcours_prof g =
+	let f (x,_) = x in 
+		f (List.fold_left 
+			(fun (p,i) s ->
+				depthProbe s g (p,i))
+			([],[]) (* initialisation de (p,i) *)
+			(liste_sommet g)
+		)
+;;
+
+let connexites g =
+	parcours_prof (inverser g)
+;;
+(*
+*)
